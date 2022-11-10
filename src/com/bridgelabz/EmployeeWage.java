b0828ad55c1e0403c381a098186ff9cfe15adfe5
Your Name <you@example.com>
2022-11-10 16:18:23 +0530

    Modified: Logic for calculation of daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,20 @@
 package com.bridgelabz;
-
 public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to employee wage computation. ");
 
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int dailyWage = 0;
         int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-        if(employeeCheck == 1) {
+        if (employeeCheck == 1) {
+            dailyWage = fullDayHour * wagePerHour;
             System.out.println("Employee is present");
         } else {
             System.out.println("Employee is absent");
         }
+        System.out.println("employee daily wage is: " + dailyWage);
     }
 }
